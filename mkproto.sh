5480393fa471f00ae221a1ab7fe3b6fb965ffcb7
Peter Schiansky <peter.schiansky@univie.ac.at>
2022-09-16 17:19:46 +0200

    Remove: Old result protobuf scheme. No longer used

--- mkproto.sh
+++ mkproto.sh
@@ -1,3 +1,2 @@
-protoc -I=./schemes --cpp_out=./src/schemes --python_out=./src/schemes ./schemes/ccset.proto
 protoc -I=./schemes --cpp_out=./src/schemes --python_out=./src/schemes ./schemes/histogramset.proto
 capnp compile -oc++:src schemes/tags.capnp
